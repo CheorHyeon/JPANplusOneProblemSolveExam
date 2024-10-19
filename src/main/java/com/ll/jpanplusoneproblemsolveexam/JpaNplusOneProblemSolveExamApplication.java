224b518289eb1f90b16119fa05ebc9601486bbba
cheorhyeon <r4560798@naver.com>
2024-10-19 16:40:29 +0900

    setting : 프로젝트 초기 세팅

---
+++ src/main/java/com/ll/jpanplusoneproblemsolveexam/JpaNplusOneProblemSolveExamApplication.java
@@ -0,0 +1,13 @@
+package com.ll.jpanplusoneproblemsolveexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaNplusOneProblemSolveExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaNplusOneProblemSolveExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/jpanplusoneproblemsolveexam/JpaNplusOneProblemSolveExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.jpanplusoneproblemsolveexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaNplusOneProblemSolveExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
